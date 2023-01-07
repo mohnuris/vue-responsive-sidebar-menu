8dcb9c3185f33464816df5892ab131303aaca1a1
MohNuris <53099995+mohnuris@users.noreply.github.com>
2023-01-07 19:56:00 +0700

    Add Router App

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@ import Sidebar from './components/Sidebar.vue'
 	--dark-alt: #334155;
 	--light: #f1f5f9;
 	--sidebar-width: 300px;
+	// --sidebar-margin;
 }
 
 * {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './router'
+// import footer from './tempalate/footer'
+// import Header from './tempalate/Header'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
